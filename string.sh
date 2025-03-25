e1836d8962dd8216c3a79eea9edeb89c17258f2f
Sruthy8139 <sruthysnair9847@gmail.com>
2025-03-25 11:29:08 +0530

    Add files via upload

---
+++ string.sh
@@ -0,0 +1,10 @@
+echo "Enter the first string:"
+read str1
+echo "Enter the second string:"
+read str2
+if [ "$str1" == "$str2" ]
+then
+echo "first and second strings are equal"
+else
+echo "first and second strings are not equal"
+fi
